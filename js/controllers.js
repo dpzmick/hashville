de7be85dbffa0a192ea6229ec9531a78f1c0e0d5
Lawrence Humphrey <lawrencehumphrey@Lawrences-MacBook-Pro-2.local>
2015-03-21 03:47:16 -0500

    pulling

--- js/controllers.js
+++ js/controllers.js
@@ -2,5 +2,6 @@ var hashControllers = angular.module('hashControllers', []);
 
 hashControllers.controller('LandingCtrl', ['$scope',
     function($scope) {
-        // code
+        $scope.where = "Where will you be staying?";
+        $scope.when = "How long will you be in Nashville?";
 }]);
