595f26d8b29afa77d18e08b872239698256c1bae
Noah Laferty <noah.lafferty@gmail.com>
2015-03-21 11:29:28 -0500

    I did some map stuff

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,19 @@ angular.module('hashControllers', ['uiGmapgoogle-maps'])
 
 .controller('LandingCtrl', ['$scope', 'uiGmapGoogleMapApi',
     function($scope, uiGmapGoogleMapApi) {
+        $scope.lat = 36.17;
+        $scope.long = -86.78;
         uiGmapGoogleMapApi.then(function(maps) {
-            $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-        });
+            $scope.map = { 
+                center: { latitude: $scope.lat, longitude: $scope.long }, 
+                zoom: 8,
+                options: {
+                    scrollwheel: false,
+//                    draggable: false,
+                    keyboardShortcuts: false,
+                    disableDoubleClickZoom: true,
+                    disableDefaultUI: true
+                }
+            };
+    });
 }]);
