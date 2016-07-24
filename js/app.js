88d4ad8fd4cd031edd58de7a49274725f1f95b70
Lawrence Humphrey <lawrenceh1993@gmail.com>
2016-07-24 18:23:54 -0500

    hopefully safe for github pages

--- js/app.js
+++ js/app.js
@@ -5,20 +5,20 @@ angular.module('hash', [
 
 .config(['$routeProvider', '$locationProvider',
     function($routeProvider, $locationProvider) {
-//        $locationProvider.html5Mode(true);
+      //  $locationProvider.html5Mode(true);
         $routeProvider
         .when('/', {
-            templateUrl: '/templates/landing.html',
+            templateUrl: 'templates/landing.html',
             controller: 'LandingCtrl'
         })
-        
+
         .when('/plan', {
-            templateUrl: '/templates/itinerary.html',
+            templateUrl: 'templates/itinerary.html',
             controller: 'ItineraryCtrl'
         })
-        
-        .otherwise({ 
-            templateUrl: '/templates/404.html' 
+
+        .otherwise({
+            templateUrl: 'templates/404.html'
         });
     }
 ]);
